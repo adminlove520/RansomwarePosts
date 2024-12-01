7e4c26ddb60c430a832d767e94a9dbbc7b8e672a
Anonymous <791751568@qq.com>
2024-12-01 14:22:01 +0800

    Update getposts.py

--- getposts.py
+++ getposts.py
@@ -34,4 +34,4 @@ for ransom in ransoms:
         ransom['group_fqdn'] = None
 
 with open('./index.html','w') as f:
-            f.write(template.render(ransoms=ransoms,fecha=dt.now.strftime('%d-%b-%Y %H:%M %Z')))
+            f.write(template.render(ransoms=ransoms,fecha=dt.now(tz=timezone.utc).strftime('%d-%b-%Y %H:%M %Z')))
